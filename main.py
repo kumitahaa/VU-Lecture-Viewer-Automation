97af147b09b635237e8423fe2b5905a429171ef1
Kumitahaa <kumitaha123@gmail.com>
2024-01-30 20:01:37 +0500

    Refactoring

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+# ------------- Import -------------
 from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.actions import mouse_button
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,7 +11,7 @@ import yaml
 
 driver = webdriver.Chrome()
 
-#Open LMS
+# ------------- Open LMS -------------
 def open_lms():
     global driver
     #  ------------- URL -------------
@@ -48,6 +46,7 @@ def check_feedback_page():
 
 
 def select_course(course_id):
+    # ------------- Select the Course to Open Lecture -------------
     gen_id = "MainContent_gvCourseList_lbtnCurrentLesson_"
     course_id = str(int(course_id)-1)
     select_course = gen_id+course_id
@@ -55,40 +54,33 @@ def select_course(course_id):
     course.click()
     
 def load_config(file_path='stu_config.yml'):
+    # ------------- Creditential in Main file -------------
     with open(file_path, 'r') as file:
         config_data = yaml.safe_load(file)
     return config_data
 
 
 def navigate_lecture():
+    # ------------- Desired Course Opened -------------
     print("Entered Navigation phase...")
     next_btn = driver.find_element(By.XPATH,"/html/body/form/div[2]/div/div[6]/div[2]/div/div/div/div[1]/div[2]/ul/li[2]/a")
-    while (True): #This loop Controls how many time it iterates
+    while (True): #This loop Controls how many time it iterates (infinite)
         quiz_complete_status = (driver.find_element(By.XPATH,"/html/body/form/div[2]/div/div[6]/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div[1]/div[1]/div[2]/span[1]")).text
         reading_complete_status = (driver.find_element(By.XPATH,"/html/body/form/div[2]/div/div[6]/div[2]/div/div/div/div[2]/div[2]/div[2]/div/div/div[1]/div[2]/span[1]")).text
         video_complete_status = (driver.find_element(By.XPATH,"/html/body/form/div[2]/div/div[6]/div[2]/div/div/div/div[2]/div[2]/div[3]/div/div[2]/span[1]")).text
-        if(quiz_complete_status == "Completed"):  #if Quiz is Complete
-            next_btn.click()
-            print("Quiz Done. Next Button has been Clicked...")
-            time.sleep(4)
-        elif(reading_complete_status == "Completed"): #if Reading is Complete
-            next_btn.click()
-            print("Reading Done. Next Button has been Clicked...")
-            time.sleep(4)
-        elif(video_complete_status == "Viewed"): #if Video is Complete
-            next_btn.click()
-            print("Video Done. Next Button has been Clicked...")
-            time.sleep(4)
-        else:
-            try:
-                next_btn_2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/form/div[2]/div/div[6]/div[2]/div/div/div/div[1]/div[2]/ul/li[2]/a")))
-                next_btn_2.click()
-                print("Clicked Next")
-            except Exception as e:
-                pass
+        count = 1
+        # ------------- Click Next Button when available -------------
+        try:
+            next_btn_2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/form/div[2]/div/div[6]/div[2]/div/div/div/div[1]/div[2]/ul/li[2]/a")))
+            next_btn_2.click()
+            count = count +1
+            print(f"Clicked Next {count}")
+        except Exception as e:
+            pass
 
-            
+
 def main():
+    # ------------- Main function -------------
     config = load_config()
     student_id = config.get('student_id', None)
     password = config.get('password', None)
@@ -102,6 +94,7 @@ def main():
     select_course(course_id)
     time.sleep(5)
     navigate_lecture()
+    print("Out of Navigation")
     time.sleep(50)
         
 
